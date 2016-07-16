67286861b7a06c7db79319179ca0a78c564e6436
kinny <1549722424@qq.com>
2016-07-16 16:31:43 +0800

    使用inspect模块反射， 获取函数定义的参数以及类型，方便以后参数检查

--- www/coroweb.py
+++ www/coroweb.py
@@ -31,6 +31,46 @@ def post(path):
 		return wrapper
 	return decorator
 
+def get_required_kw_args(fn):
+	args = []
+	params = inspect.signature(fn).parameters
+	for name, param in params.items():
+		if param.kind == inspect.Parameter.KEYWORD_ONLY and param.default == inspect.Parameter.empty:
+			args.append(name)
+	return tuple(args)
+
+def get_named_kw_args(fn):
+	args = []
+	params = inspect.signature(fn).parameters
+	for name, param in params.items():
+		if param.kind == inspect.Parameter.KEYWORD_ONLY:
+			args.append(name)
+	return tuple(args)
+
+def has_named_kw_args(fn):
+	params = inspect.signature(fn).parameters
+	for name, param in params.items():
+		if param.kind == inspect.Parameter.KEYWORD_ONLY:
+			return True
+
+def has_var_kw_arg(fn):
+	params = inspect.signature(fn).parameters
+	for name, param in params.items():
+		if param.kind == inspect.Parameter.VAR_KEYWORD:
+			return True
+
+def has_request_arg(fn):
+	sig = inspect.signature(fn)
+	params = sig.parameters
+	found = False
+	for name, param in params.items():
+		if name == 'request':
+			found = True
+			continue
+		if found and (param.kind != inspect.Parameter.VAR_POSITIONAL and param.kind != inspect.Parameter.KEYWORD_ONLY and param.kind != inspect.Parameter.VAR_KEYWORD):
+			raise ValueError('request parameter must be the last named parameter in function: %s%s' % (fn.__name__, str(sig)))
+	return found
+
 def add_static(app):
 	'''
 		Add static resources route
@@ -71,5 +111,3 @@ def add_routes(app, module_name):
 			path = getattr(fn, '__route__', None)
 			if method and path:
 				add_route(app, fn)
-
-	'''	
