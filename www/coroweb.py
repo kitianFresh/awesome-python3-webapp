1c9612993097e071facf8afdfa544896e515acc5
kinny <1549722424@qq.com>
2016-07-16 14:19:20 +0800

    编写web框架，定义了两个装饰器@get和@post

---
+++ www/coroweb.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import asyncio, os, inspect, logging, functools
+
+from aiohttp import web
+
+def get(path):
+	'''
+		Define decorator @get('/path')
+	'''
+	def decorator(func):
+		@functools.wraps(func)
+		def wrapper(*args, **kw):
+			return func(*args, **kw)
+		wrapper.__method__ = 'GET'
+		wrapper.__route__ = path
+		return wrapper
+	return decorator
+
+def post(path):
+	'''
+		Define decorator @post('/path')
+	'''	
+	def decorator(func):
+		@functools.wraps(func)
+		def wrapper(*args, **kw):
+			return func(*args, **kw)
+		wrapper.__method__ = 'POST'
+		wrapper.__route__ = path
+		return wrapper
+	return decorator
